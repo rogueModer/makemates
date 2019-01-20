198cc26cf5522a3cd8ddde5d48f8af84d3702343
rogueModer <arush3339@gmail.com>
2019-01-20 07:49:05 +0530

    I am just checking file Title name

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 
 	require_once __DIR__ . "/includes/header.php";
 	require_once __DIR__ . "/init.php";
-
+	
 	if(!LOGIN::isLoggedIn()){
-
 ?>
 
 	<nav class="navbar navbar-expand-md position-fixed" id="myHeader">
